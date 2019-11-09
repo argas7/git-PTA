52fd0b4fc5d013fd961a11536d5f366229103d5a
argas7 <pgsr@cin.ufpe.br>
2019-11-09 16:14:25 -0300

    second commit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class People(models.Model):
+    name = models.CharField('Nome completo', max_length=100, null=True, blank=True)
+    cpf = models.IntegerField('CPF', primary_key=True, unique=True)
+    email = models.EmailField("E-mail", null=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+
+    def __str__(self):
+        return self.name
